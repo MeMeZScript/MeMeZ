6a6c889a01df69610fae8fa7ff3107a59a2253bc
MeMeZ <memezscript@outlook.com>
2025-01-20 11:44:33 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,3 @@
+local Supports = {}
+local Scriptname = Supports[game.PlaceId] or "Universal.lua"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MeMeZScript/MeMeZ/refs/heads/main/Scripts/"..Scriptname))()
